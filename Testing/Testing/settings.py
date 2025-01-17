071b21a2d043571ae23f6ecea5d9eb9193e2e991
milind.rathod <milind.rathod@rkfoodland.com>
2025-01-17 12:20:48 +0530

    add / in setting

--- Testing/Testing/settings.py
+++ Testing/Testing/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 import os
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
