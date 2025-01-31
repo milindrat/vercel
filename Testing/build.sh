bce84280fd819d5995933c8c811bd30c24093049
milind.rathod <milind.rathod@rkfoodland.com>
2025-01-31 17:37:01 +0530

    rt13

--- Testing/build.sh
+++ Testing/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -o errexit   # Exit on the first error
 
-PORT=${PORT:-8000}
+
 # Install dependencies from requirements.txt
 pip install -r requirements.txt
 
@@ -11,4 +11,4 @@ python manage.py collectstatic --no-input
 # Apply database migrations
 python manage.py migrate
 
-exec gunicorn Testing/Testing.wsgi:application --bind 0.0.0.0:$PORT
+gunicorn Testing.wsgi:application
