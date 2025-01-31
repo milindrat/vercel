18ea86b54af055fb86d5ae422be74e863b233019
milind.rathod <milind.rathod@rkfoodland.com>
2025-01-31 16:11:53 +0530

    rtmt

--- Testing/Testing/settings.py
+++ Testing/Testing/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhitenoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  
 ]
 
 ROOT_URLCONF = 'Testing.urls'
