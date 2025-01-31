12e3fe5d48b5af0e856f513efc8c3e66663a71e8
milind.rathod <milind.rathod@rkfoodland.com>
2025-01-31 17:51:01 +0530

    rt14

--- Testing/build.sh
+++ Testing/build.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 set -o errexit   # Exit on the first error
 
-
+python -m venv myenv
+myenv\Scripts\activate
 # Install dependencies from requirements.txt
 pip install -r requirements.txt
 
 # Collect static files without prompting for input
-python manage.py collectstatic --no-input
+python manage.py collectstatic 
 
 # Apply database migrations
 python manage.py migrate
